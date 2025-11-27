a56bd41e4177bddbc1b4d8ccc72b4a9ae305ec45
SarahWooller <SarahWooller@users.noreply.github.com>
2025-11-27 11:10:23 +0000

    Ssecret Santa 2025

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/secret_santa/",
+  build: {
+    // CRITICAL: Change output directory from 'dist' to 'docs'
+    outDir: 'docs'
+  },
   plugins: [
     // Ensure the React plugin is configured for both .js and .jsx files
     react({
