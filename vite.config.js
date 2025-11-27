505212e93a7ef61de221a908178a1fa2766cebd4
SarahWooller <SarahWooller@users.noreply.github.com>
2025-11-27 13:19:44 +0000

    Ssecret Santa 2025

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+  build: {
+    // CRITICAL: Change output directory from 'dist' to 'docs'
+    outDir: 'docs'
+  },
+  plugins: [
+    // Ensure the React plugin is configured for both .js and .jsx files
+    react({
+      include: '**/*.{jsx,js}',
+    })
+  ],
+});
