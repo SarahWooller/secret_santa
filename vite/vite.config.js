fda6b079786ed0f1b4f0edcd41e6ac8e23cecddc
SarahWooller <SarahWooller@users.noreply.github.com>
2025-11-27 10:59:34 +0000

    Ssecret Santa 2025

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/secret_santa/",
   plugins: [
     // Ensure the React plugin is configured for both .js and .jsx files
     react({
